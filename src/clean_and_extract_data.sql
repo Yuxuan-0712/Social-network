a248c2fc00e1fa15ffb55bfaf4d75547258bf977
Yuxuan-0712 <E1353417@u.nus.edu>
2026-03-02 10:44:33 +0800

    Update clean_and_extract_data.sql

--- src/clean_and_extract_data.sql
+++ src/clean_and_extract_data.sql
@@ -17,25 +17,57 @@ CREATE TABLE edges_table(
 INSERT INTO edges_table(
   timestamp, source, target
 )
-SELECT DISTINCT ON(src, tgt) tsp, src, tgt
-FROM edges_stage
-ORDER BY tgt, src, tsp DESC;
 
-CREATE TABLE node_degree AS
+
+# create the birth_size column
+ALTER TABLE edges_table ADD COLUMN "size" INTEGER;
+UPDATE edges_table
+SET size = result.size
+FROM(
+  SELECT tgt,
+  src,
+  tsp,
+  COUNT(DISTINCT node) OVER (ORDER BY tgt,src,tsp ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS size
+  FROM (
+  SELECT tgt,
+  src,
+  tsp
+  FROM edges_table
+  ORDER BY tgt, src, tsp
+  ) AS edge
+  CROSS JOIN LATERAL 
+  (SELECT src AS node UNION ALL SELECT tgt) AS nodes
+) AS results
+WHERE edges_table.tgt = results.tgt
+AND edges_table.src = results.src
+AND edges_table.tsp = results.tsp
+
+CREATE TABLE node_property AS
 WITH outdeg AS (
   SELECT source AS node, COUNT(*) AS outdegree
   FROM edges_table
   GROUP BY source
-)
+),
 indeg AS (
   SELECT target AS node, COUNT(*) AS indegree
   FROM edges_table
   GROUP BY target
 )
+birth_size AS (
+  SELECT src AS node, MIN(size) AS birth_size
+  FROM edges_table
+  GROUP BY src
+  
+  UNION ALL
+  
+  SELECT tgt AS node, MIN(size) AS birth_size
+  FROM edges_table
+  GROUP BY tgt
+)
 SELECT 
-  COALESCE (i.node, o.node)
-  COALESCE (i.indegree, 0)
-  COALESCE (0,o.outdegree)
+  COALESCE (i.node, o.node) AS node,
+  COALESCE (i.indegree, 0) AS indegree,
+  COALESCE (o.outdegree,0) AS outdegree
 FROM outdeg o
 FULL OUTER JOIN indeg i
-o.node = i.node,
+ON o.node = i.node,
